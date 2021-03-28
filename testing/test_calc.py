101104d9ca90aff1f9026a5cc7f378db3283ef76
lilili-z <1056747375@qq.com>
2021-03-28 19:00:50 +0800

    allure

--- testing/test_calc.py
+++ testing/test_calc.py
@@ -2,6 +2,7 @@
 测试用例
 用来测试业务代码calc-practice
 """
+import allure
 import pytest
 
 from python_code.calc import Calculator
@@ -16,7 +17,7 @@ from python_code.calc import Calculator
 #     div_datas=datas_div['datas']
 #     div_myid =datas_div['myid']
 
-
+@allure.feature("测试浏览器")
 class TestCalc:
 
     # def setup(self):
@@ -30,13 +31,16 @@ class TestCalc:
     #     "a,b,expect",
     #     add_datas,  ids=myid
     # )
+    @allure.story("测试加法")
+    @allure.title("测试加法")
     def test_add(self, get_calc, get_add_datas):
         # 实例化计算器类
         # calc = Calculator()
         # 调用add方法传入值
         result = None
         try:
-            result = get_calc.add(get_add_datas[0], get_add_datas[1])
+            with allure.step("输入两个值"):
+                result = get_calc.add(get_add_datas[0], get_add_datas[1])
             if isinstance(result, float):
                 result = round(result, 2)
         except Exception as e:
@@ -49,10 +53,12 @@ class TestCalc:
     #      "a1,b1,expect1",
     #      div_datas,  ids=div_myid
     #  )
+    @allure.story("测试减法")
     def test_div(self, get_calc, get_div_datas):
         result = None
         try:
-            result = get_calc.div(get_div_datas[0], get_div_datas[1])
+            with allure.step("输入两个值"):
+                result = get_calc.div(get_div_datas[0], get_div_datas[1])
             if isinstance(result, float):
                 result = round(result, 2)
         except Exception as e:
